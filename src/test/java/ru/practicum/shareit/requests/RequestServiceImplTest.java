2cbf05905017f5ccef75da9eac9bd66cf0e4098d
Stan <sdord@yandex.ru>
2022-12-09 02:42:14 +0300

    fix super style

--- src/test/java/ru/practicum/shareit/requests/RequestServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/requests/RequestServiceImplTest.java
@@ -22,7 +22,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 
 @Transactional
